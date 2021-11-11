b70a40ff622f018b9d5f73d23bee117d8df72497
kyh1116 <kjhjhj731@naver.com>
2021-11-11 21:51:41 +0900

    UI_code

---
+++ sole.js
@@ -0,0 +1,10 @@
+const locationForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#result')
+
+locationForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    messageOne.textContent = location
+    
+})
